b7a407c29f981bc4bacf3ba6fe317807d88865dd
user.name "xxrobone <user.email "robert.wagar@edu.cmeducations.se>
2023-04-14 14:43:37 +0200

    nav and btn fixes, after merge

--- menu_btn.js
+++ menu_btn.js
@@ -1,5 +1,8 @@
+let menuBtn = document.querySelector('#hamburger');
+let menu = document.querySelector('nav > ul');
+
 menuBtn.addEventListener('click', () => {
-    console.log('clicked button');
-    menuBtn.classList.toggle('active');
-    menu.classList.toggle('open');
-  });
+  console.log('clicked button');
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('open');
+});
